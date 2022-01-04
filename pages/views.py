22dd34389d492f1bc379f83a2375ea663b9bc4f8
gaeun6883 <93725108+gaeun6883@users.noreply.github.com>
2022-01-04 23:37:26 +0900

    Create views.py

---
+++ pages/views.py
@@ -0,0 +1,9 @@
+
+# Create your views here.
+from django.shortcuts import render
+
+def mainpage(request):
+    return render(request, 'pages/mainpage.html')
+
+def company(request):
+    return render(request, 'pages/company_info.html')
