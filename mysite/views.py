5eba8ee69c807bb281e9700154e58d3c910dbc5b
gaeun6883 <93725108+gaeun6883@users.noreply.github.com>
2022-02-11 15:50:04 +0900

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -2,6 +2,7 @@ from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import MainContent, Comment
+from .models import Post, Photo
 from .forms import CommentForm
 
 @login_required(login_url='accounts:login')
@@ -60,5 +61,26 @@ def comment_create(request, content_id):
         context = {'content_list': content_list, 'form': form}
         return render(request, 'mysite/content_detail.html', context)
 
+def create(request):
+    if(request.method == 'POST'):
+        post = Post()
+        post.title = request.POST['title']
+        post.content = request.POST['content']
+        post.pub_date = timezone.datetime.now()
+        post.user = request.user
+        post.save()
+        # name 속성이 imgs인 input 태그로부터 받은 파일들을 반복문을 통해 하나씩 가져온다
+        for img in request.FILES.getlist('imgs'):
+            # Photo 객체를 하나 생성한다.
+            photo = Photo()
+            # 외래키로 현재 생성한 Post의 기본키를 참조한다.
+            photo.post = post
+            # imgs로부터 가져온 이미지 파일 하나를 저장한다.
+            photo.image = img
+            # 데이터베이스에 저장
+            photo.save()
+        return redirect('/detail/' + str(post.id))
+    else:
+        return render(request, 'new.html')
 
 
